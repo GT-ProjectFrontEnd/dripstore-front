1efdc0dde5434fd989fd877e807f8fc1ca78dffe
victorhelio66 <heliovictor180@gmail.com>
2024-07-21 19:43:16 -0300

    front-end header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Header } from "C:/Users/helio/OneDrive/Área de Trabalho/drip store/dripstore-front/src/components/Header/Header.jsx";
+import {Routes, Route } from 'react-router-dom'
 import './App.css'
 
 export function App() {
 
   return (
     <>
-    <h1>Tá funcionando!</h1>
+      <Header></Header>
+
+    {/* <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes> */}
+     <h1>Tá funcionando!</h1>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,43 @@
-import './Header.css'
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export function Header() {
-
-    return (
-      <>
-    
-      </>
-    )
-  }
-  
+  return (
+    <header className="header-f">
+      <section className="header-main">
+        <img className="logo" src="src/image/logo-dc.png" alt="Digital Store" />
+        <div className="search">
+          <input type="text" name="" id="" placeholder="Pesquisar produto..." />
+          <img src="src/image/lupa.png" alt=""></img>
+        </div>
+        <div className="register">
+          <a href="">Cadastre-se</a>
+          <div className="button-container"><a href="#"><button className="button-login">Entrar</button></a> </div>
+        </div>
+        <div className="cart">
+          <img src="src/image/carrinho.png" alt="" />
+        </div>
+      </section>
+      <nav className="nav">
+      
+        <li>
+          <Link to="/" className="link">Home</Link>
+          <div></div>
+        </li>
+        <li>
+          <Link to="/Products" className="link">Produtos</Link>
+          <div></div>
+        </li>
+        <li>
+          <Link to="/Categories" className="link">Categorias</Link>
+          <div></div>
+        </li>
+        <li>
+          <Link to="/Requests" className="link">Meus pedidos</Link>
+          <div></div>
+        </li>
+      
+    </nav>
+    </header>
+  );
+}
