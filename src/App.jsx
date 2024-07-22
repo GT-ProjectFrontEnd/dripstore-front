071cafac988d4297206ec046e2f90530c31f7212
victorhelio66 <heliovictor180@gmail.com>
2024-07-22 09:31:05 -0300

    footer final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { Header } from "C:/Users/helio/OneDrive/Área de Trabalho/drip store/dripstore-front/src/components/Header/Header.jsx";
-import { Footer } from "./components/Footer/Footer";
 import {Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -7,14 +5,10 @@ export function App() {
 
   return (
     <>
-      <Header></Header>
-      
-
     {/* <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes> */}
      <h1>Tá funcionando!</h1>
-     <Footer></Footer>
     </>
   )
 }
