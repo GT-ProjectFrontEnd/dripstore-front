24ae05eed0d5f8f6ac5f7bf8c72d4fae80a398bc
WanessaT <wanessatlsousa@gmail.com>
2024-07-22 16:37:06 -0300

    Ajustando posicionamento dos filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { ProductListing } from './pages/ProductListing/ProductListing'
 
 export function App() {
 
   return (
     <>
-
+    <ProductListing />
     </>
   )
 }
