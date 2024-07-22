a63a5785f81d8e2add27a986919fb1f048268016
Wanessa :) <145905932+WanessaT@users.noreply.github.com>
2024-07-22 11:51:45 -0300

    Revert "Meus pedidos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import {Routes, Route } from 'react-router-dom'
 import './App.css'
-import { MeusPedidos } from './components/MeusPedidos/MeusPedidos'
 
 export function App() {
 
   return (
     <>
-      <MeusPedidos />
-=======
     {/* <Routes>
       <Route path='/' element={<Home/>}/>
     </Routes> */}
