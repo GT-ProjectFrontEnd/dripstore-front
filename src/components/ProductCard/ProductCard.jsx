75cec41f707645b6172f79f5038b77ae6a697cb7
Bruno Saraiva <bruno_saraiva@outlook.com.br>
2024-07-25 16:58:57 -0300

    cores

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,90 +1,94 @@
+
+import { Footer } from '../Footer/Footer'
+import { Header } from '../Header/Header'
 import './ProductCard.css'
 
-export function ProductCard(){
+export function ProductCard() {
 
     return (
-      <>
-    <body>
-    <main>
-        <section class="cart">
-            <div class="container">
-                <h2>Meu Carrinho</h2>
-                <div class="cart-items">
-                    <div class="cart-item">
-                        {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
-                        <div class="cart-item-info">
-                            <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
-                            <p>Cor: Vermelho / Branco</p>
-                            <p>Tamanho: 42</p>
+        <>
+            <Header />
+            <body>
+                <main>
+                    <section class="cart">
+                        <div class="container">
+                            <h2>Meu Carrinho</h2>
+                            <div class="cart-items">
+                                <div class="cart-item">
+                                    {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
+                                    <div class="cart-item-info">
+                                        <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
+                                        <p>Cor: Vermelho / Branco</p>
+                                        <p>Tamanho: 42</p>
+                                    </div>
+                                    <div class="cart-item-actions">
+                                        <button class="decrease">-</button>
+                                        <input type="number" value="1" min="1" class="quantity" />
+                                        <button class="increase">+</button>
+                                        <span class="price">R$ 219,00</span>
+                                        <button class="remove-item">Remover Item</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cart-summary">
+                                <h3>Resumo</h3>
+                                <ul>
+                                    <li>Subtotal: <span id="subtotal">R$ 0,00</span></li>
+                                    <li>Frete: <span id="frete">R$ 0,00</span></li>
+                                    <li>Desconto: <span id="desconto">R$ 30,00</span></li>
+                                    <li>Total: <span id="total" class="blink">R$ 219,00</span></li>
+                                </ul>
+                                <div class="coupon">
+                                    <h4>Cupom de desconto</h4>
+                                    <input type="text" placeholder="Insira seu código" class="coupon-code" />
+                                    <button class="btn">OK</button>
+                                </div>
+                                <div class="shipping">
+                                    <h4>Calcular frete</h4>
+                                    <input type="text" placeholder="Insira seu CEP" class="shipping-cep" />
+                                    <button class="btn">OK</button>
+                                </div>
+                                <button class="btn" id="continue">Continuar</button>
+                            </div>
                         </div>
-                        <div class="cart-item-actions">
-                            <button class="decrease">-</button>
-                            <input type="number" value="1" min="1" class="quantity">
-                            <button class="increase">+</button>
-                            <span class="price">R$ 219,00</span>
-                            <button class="remove-item">Remover Item</button>
+                    </section>
+
+                    <section class="related-products">
+                        <div class="container">
+                            <h2>Produtos Relacionados</h2>
+                            <div class="products">
+                                <div class="product">
+                                    <div class="discount">30% OFF</div>
+                                    {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
+                                    <h3>Tênis</h3>
+                                    <h4>K-Swiss V8 - Masculino</h4>
+                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
+                                </div>
+                                <div class="product">
+                                    {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
+                                    <h3>Tênis</h3>
+                                    <h4>K-Swiss V8 - Masculino</h4>
+                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
+                                </div>
+                                <div class="product">
+                                    {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
+                                    <h3>Tênis</h3>
+                                    <h4>K-Swiss V8 - Masculino</h4>
+                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
+                                </div>
+                                <div class="product">
+                                    {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
+                                    <h3>Tênis</h3>
+                                    <h4>K-Swiss V8 - Masculino</h4>
+                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
+                                </div>
+                            </div>
+                            <a href="#" class="btn">Ver todos →</a>
                         </div>
-                    </div>
-                </div>
-                <div class="cart-summary">
-                    <h3>Resumo</h3>
-                    <ul>
-                        <li>Subtotal: <span id="subtotal">R$ 0,00</span></li>
-                        <li>Frete: <span id="frete">R$ 0,00</span></li>
-                        <li>Desconto: <span id="desconto">R$ 30,00</span></li>
-                        <li>Total: <span id="total" class="blink">R$ 219,00</span></li>
-                    </ul>
-                    <div class="coupon">
-                        <h4>Cupom de desconto</h4>
-                        <input type="text" placeholder="Insira seu código" class="coupon-code">
-                        <button class="btn">OK</button>
-                    </div>
-                    <div class="shipping">
-                        <h4>Calcular frete</h4>
-                        <input type="text" placeholder="Insira seu CEP" class="shipping-cep">
-                        <button class="btn">OK</button>
-                    </div>
-                    <button class="btn" id="continue">Continuar</button>
-                </div>
-            </div>
-        </section>
-
-        <section class="related-products">
-            <div class="container">
-                <h2>Produtos Relacionados</h2>
-                <div class="products">
-                    <div class="product">
-                        <div class="discount">30% OFF</div>
-                         {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
-                        <h3>Tênis</h3>
-                        <h4>K-Swiss V8 - Masculino</h4>
-                        <p><s>R$ 200</s> <span>R$ 100</span></p>
-                    </div>
-                    <div class="product">
-                         {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
-                        <h3>Tênis</h3>
-                        <h4>K-Swiss V8 - Masculino</h4>
-                        <p><s>R$ 200</s> <span>R$ 100</span></p>
-                    </div>
-                    <div class="product">
-                         {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
-                        <h3>Tênis</h3>
-                        <h4>K-Swiss V8 - Masculino</h4>
-                        <p><s>R$ 200</s> <span>R$ 100</span></p>
-                    </div>
-                    <div class="product">
-                         {/* <!-- <img src="tenisimagem" alt="Tênis Nike Revolution 6 Next Nature Masculino"> --> */}
-                        <h3>Tênis</h3>
-                        <h4>K-Swiss V8 - Masculino</h4>
-                        <p><s>R$ 200</s> <span>R$ 100</span></p>
-                    </div>
-                </div>
-                 <a href="#" class="btn">Ver todos →</a>
-            </div>
-        </section>
-    </main>
-</body>
-
-      </>
+                    </section>
+                </main>
+            </body>
+            <Footer />
+        </>
     )
-  }
+}
--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,134 +1,58 @@
-// import './src./Header'
-// import './src./Footer'
+import {Header} from '../Header/Header'
+import {Footer} from '../Footer/Footer'
 import './ProductDetails.css'
 
 export function ProductDetails(){
 
-  
+
 return (
   <>
-  <div class="container">
-    <div class="content">
-      <div class="product">
+  <Header/>
+  <div className="container">
+    <div className="content">
+      <div className="product">
         <img src='../src/image/air-force-detail.png'/>
-          <div class="details">
+          <div className="details">
             <h1>Tênis Nike Revolution 6 Next Nature Masculino</h1>
             <p>Casual | Nike | REF: 38416711</p>
-            <div class="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star-half-alt"></i>
+            <div className="rating">
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star"></i>
+              <i className="fas fa-star-half-alt"></i>
               <span>(90 avaliações)</span>
             </div>
-            <div class="price">
+            <div className="price">
               R$ 219,00
-              <span class="old">R$ 210,00</span>
+              <span className="old">R$ 210,00</span>
             </div>
-            {/* <!-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p> --> */}
-            <div class="size">
-              <div class="option" data-size="39">39</div>
-              <div class="option" data-size="40">40</div>
-              <div class="option selected" data-size="41">41</div>
-              <div class="option" data-size="42">42</div>
-              <div class="option" data-size="43">43</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            <p>Tamanhos</p>
+            <div className="size">
+              <div className="option" data-size="39">39</div>
+              <div className="option" data-size="40">40</div>
+              <div className="option selected" data-size="41">41</div>
+              <div className="option" data-size="42">42</div>
+              <div className="option" data-size="43">43</div>
             </div>
-            <button class="button">COMPRAR</button>
+            <div>
+              <p>Cores</p>
+            </div>
+            <div className='qualquer'>
+              <div className='color_unid' id= 'ciano'></div>
+              <div className='color_unid' id= 'rosa'></div>
+              <div className='color_unid' id='preto' ></div>
+              <div className='color_unid'id='roxo'></div>
+            </div>
+            <button className="button">COMPRAR</button>
           </div>
-      </div>
 
-      <div class="related-products">
-        <h2 class="title">Produtos Relacionados</h2>
-        <div class="products">
-          <div class="product">
-            {/* <!-- <img src="kswiss" alt="K-Swiss V8 - Masculino" class="image"> --> */}
-            <div class="details">
-              <h2>K-Swiss V8 - Masculino</h2>
-              <div class="price">
-                <span class="old">R$ 200</span>
-                R$ 100
-              </div>
-            </div>
-          </div>
-          <div class="product">
-            {/* <!-- <img src="tenis k-swiss" alt="K-Swiss V8 - Masculino" class="image"> --> */}
-            <div class="details">
-              <h2>K-Swiss V8 - Masculino</h2>
-              <div class="price">
-                <span class="old">R$ 200</span>
-                R$ 100
-              </div>
-            </div>
-          </div>
-          <div class="product">
-            {/* <!-- <img src="K-Swiss V8" alt="K-Swiss V8 - Masculino" class="image"> --> */}
-            <div class="details">
-              <h2>K-Swiss V8 - Masculino</h2>
-              <div class="price">
-                <span class="old">R$ 200</span>
-                R$ 100
-              </div>
-            </div>
-          </div>
-          <div class="product">
-            {/* <!-- <img src="k-swiss" alt="K-Swiss V8 - Masculino" class="image"> --> */}
-            <div class="details">
-              <h2>K-Swiss V8 - Masculino</h2>
-              <div class="price">
-                <span class="old">R$ 200</span>
-                R$ 100
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
+      {/* ADD CARROSSEL */}
     </div>
-
-    {/* <footer class="footer">
-      <div class="container">
-        <div class="column">
-          <h3>► Digital Store</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-          <ul class="social">
-            <!-- <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li> -->
-          </ul>
-        </div>
-        <div class="column">
-          <h3>Informação</h3>
-          <ul>
-            <!-- <li><a href="#">Sobre Drip Store</a></li>
-            <li><a href="#">Segurança</a></li>
-            <li><a href="#">Wishlist</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Trabalhe conosco</a></li>
-            <li><a href="#">Meus Pedidos</a></li> -->
-          </ul>
-        </div>
-        <div class="column">
-          <h3>Categorias</h3>
-          <ul>
-            <!-- <li><a href="#">Camisetas</a></li>
-            <li><a href="#">Calças</a></li>
-            <li><a href="#">Bonés</a></li>
-            <li><a href="#">Headphones</a></li>
-            <li><a href="#">Tênis</a></li> -->
-          </ul>
-        </div>
-        <div class="column">
-          <h3>Contato</h3>
-          <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
-          <p>(85) 3051-3411</p>
-        </div>
-      </div>
-      <div class="copyright">
-        <p>&copy; 2022 Digital College</p>
-      </div>
-    </footer> */}
-
   </div>
+  <Footer/>
   </>
   )
 }
