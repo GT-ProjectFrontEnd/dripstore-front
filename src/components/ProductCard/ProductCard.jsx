c7ddc879af4fb34766b298d8014b72d8700be1cf
victorhelio66 <heliovictor180@gmail.com>
2024-07-26 02:59:37 -0300

    alteraçoes na ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -10,80 +10,83 @@ export function ProductCard() {
             <Header />
             <body>
                 <main>
-                    <section class="cart">
-                        <div class="container">
-                            <h2>Meu Carrinho</h2>
-                            <div class="cart-items">
-                                <div class="cart-item">
-                                    <img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/>
-                                    <div class="cart-item-info">
+                    <section className="cart-1">
+                        <div className="container-1">
+                            <div className='t1'>
+                                <h2>Meu Carrinho</h2>
+                                <div> 
+                                    <p>QUANTIDADE</p>
+                                    <p>UNITÁRIO</p>
+                                    <p>TOTAL</p>
+                                </div>
+                            </div>
+                            
+                            <div className="cart-items">
+                                <div className="cart-item">
+                                    <figure><img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/></figure> 
+                                    <div className="cart-item-info">
                                         <h3>Tênis Nike Revolution 6 Next Nature Masculino</h3>
                                         <p>Cor: Vermelho / Branco</p>
                                         <p>Tamanho: 42</p>
                                     </div>
-                                    <div class="cart-item-actions">
-                                        <button class="decrease">-</button>
-                                        <input type="number" value="1" min="1" class="quantity" />
-                                        <button class="increase">+</button>
-                                        <span class="price">R$ 219,00</span>
-                                        <button class="remove-item">Remover Item</button>
+                                </div>
+                                <div className="cart-item-actions">
+                                    <div>
+                                        <div> <button className="decrease">-</button>
+                                            <input type="number" value="1" min="1" className="quantity" />
+                                            <button className="increase">+</button>
+                                        </div>
+                                        <button className="remove-item">Remover Item</button> 
                                     </div>
+                                    <span className="price">R$ 219,00</span>
+                                    <span className="price">R$ 219,00</span>
+                                     
+                                </div>
+                                
+                                    
+                            </div>
+
+                            <div className='desc'>
+                                <div className="coupon">
+                                    <h4>Cupom de desconto</h4>
+                                    <div className='inptDF'> <input type="text" placeholder="Insira seu código" className="coupon-code" />
+                                    <button className="btn">OK</button>
+                                    </div>
+                                   
+                                </div>
+                                <div className="shipping">
+                                    <h4>Calcular frete</h4>
+                                    <div className='inptDF'><input type="text" placeholder="Insira seu CEP" className="shipping-cep" />
+                                    <button className="btn">OK</button>
+                                    </div>
+                                    
                                 </div>
                             </div>
-                            <div class="cart-summary">
+                          
+                        </div>
+                        <div className="cart-summary">
                                 <h3>Resumo</h3>
                                 <ul>
                                     <li>Subtotal: <span id="subtotal">R$ 0,00</span></li>
                                     <li>Frete: <span id="frete">R$ 0,00</span></li>
                                     <li>Desconto: <span id="desconto">R$ 30,00</span></li>
-                                    <li>Total: <span id="total" class="blink">R$ 219,00</span></li>
+                                    <li>Total: <span id="total" className="blink">R$ 219,00</span></li>
                                 </ul>
-                                <div class="coupon">
-                                    <h4>Cupom de desconto</h4>
-                                    <input type="text" placeholder="Insira seu código" class="coupon-code" />
-                                    <button class="btn">OK</button>
-                                </div>
-                                <div class="shipping">
-                                    <h4>Calcular frete</h4>
-                                    <input type="text" placeholder="Insira seu CEP" class="shipping-cep" />
-                                    <button class="btn">OK</button>
-                                </div>
-                                <button class="btn" id="continue">Continuar</button>
-                            </div>
+                                
+                                <button className="btn" id="continue">Continuar</button>
                         </div>
+                        
                     </section>
 
-                    <section class="related-products">
-                        <div class="container">
-                            <h2>Produtos Relacionados</h2>
-                            <div class="products">
-                                <div class="product">
-                                    <div class="discount">30% OFF</div>
-                                      <img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/>
-                                    <h3>Tênis</h3>
-                                    <h4>K-Swiss V8 - Masculino</h4>
-                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
-                                </div>
-                                <div class="product">
-                                      <img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/>
-                                    <h3>Tênis</h3>
-                                    <h4>K-Swiss V8 - Masculino</h4>
-                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
-                                </div>
-                                <div class="product">
-                                      <img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/>
-                                    <h3>Tênis</h3>
-                                    <h4>K-Swiss V8 - Masculino</h4>
-                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
-                                </div>
-                                <div class="product">
-                                <img src="../src/image/tenis_revolution.png" alt="Tênis Nike Revolution 6 Next Nature Masculino"/>
-                                    <h3>Tênis</h3>
-                                    <h4>K-Swiss V8 - Masculino</h4>
-                                    <p><s>R$ 200</s> <span>R$ 100</span></p>
-                                </div>
+                    <section className="related-products">
+                        <div className="container">
+                            <div className='vtodos'><h2>Produtos Relacionados</h2>
+                            <a href="#" className="btn">Ver todos →</a>
                             </div>
-                            <a href="#" class="btn">Ver todos →</a>
+                            
+                            <div className="products">
+                            </div>
+                            
                         </div>
                     </section>
                 </main>
